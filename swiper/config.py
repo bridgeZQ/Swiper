b377b6c5ca4b49f1ae772d5dac51d197f9053da5
bridgeZQ <15779062307@163.com>
2019-04-01 19:12:58 +0800

    1.创建了User模型. 2.实现了发送短信验证码的接口. 3.封装了返回json的接口.

---
+++ swiper/config.py
@@ -0,0 +1,14 @@
+"""存放第三方配置"""
+
+
+# 云之讯
+YZX_SMS_API = 'https://open.ucpaas.com/ol/sms/sendsms'
+
+YZX_SMS_PARAMS = {
+     "sid": "a988d01b750480db023bcf020b3c57ab",
+     "token": "46cb2efddaf07e79d2a8ad3c97149af8",
+     "appid": "fdaed3bed1204040982e5c09218a7ac7",
+     "templateid": "449580",
+     "param": None,
+     "mobile": None,
+}
