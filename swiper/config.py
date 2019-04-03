332012965fd30502567c729f3dbd7047804c8341
bridgeZQ <42398770+bridgeZQ@users.noreply.github.com>
2019-04-03 21:36:13 +0800

    V1.0.0 (#2)
    创建了User模型.
    实现了发送短信验证码的接口.
    封装了返回json的接口.
    完成登录接口.
    完成用户序列化to_dict方法.
    封装errors和keys.
    新增了profile模型.
    实现了个人资料API.
    封装ModelMixin类.
    实现修改个人资料API.
    实现上传头像API.
    使用celery实现耗时任务的异步操作.
    使用redis做缓存.

---
+++ swiper/config.py
@@ -0,0 +1,20 @@
+"""存放第三方配置"""
+
+
+# 云之讯
+YZX_SMS_API = 'https://open.ucpaas.com/ol/sms/sendsms'
+
+YZX_SMS_PARAMS = {
+     "sid": "a988d01b750480db023bcf020b3c57ab",
+     "token": "46cb2efddaf07e79d2a8ad3c97149af8",
+     "appid": "fdaed3bed1204040982e5c09218a7ac7",
+     "templateid": "449580",
+     "param": None,
+     "mobile": None,
+}
+
+# 七牛云配置
+QN_ACCESS_KEY = 'B9TlQdWYREuT_Z8LyT3h35C6hV7LlFXDpZG2THRx'
+QN_SECRET_KEY = 'bXAfnuOoBcC6qQf3W7l5q66yJwncozlQcbehVjNi'
+QN_BUCKET_NAME = 'swiper'
+QN_CLOUD_URL = 'http://ppd4wuy6o.bkt.clouddn.com'
